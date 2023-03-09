f19e840030b989138535610a779f3da90134a100
gonzalo-novak <monstertrap@protonmail.com>
2023-03-08 23:52:17 -0600

    chore: install tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./(layout|sections|snippets)/*.liquid"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
